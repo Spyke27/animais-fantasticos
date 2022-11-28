f46d52d003d2e2e2252241c95b3baefd5adca39a
Rodrigo Daniel <rodrigoeufrasio2@gmail.com>
2022-11-27 22:49:04 -0300

    scroll suave da classe animais

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const animais = document.getElementById('animais')
-const gridSection = document.querySelectorAll('.grid-section')
+const tabMenu = document.querySelectorAll('.js-tabmenu li')
+const tabCont = document.querySelectorAll('.js-tabcont section')
 
+function activeTab(index){
+    tabCont.forEach((section) => {
+        section.classList.remove('ativo');
+    });
+    tabCont[index].classList.add('ativo');
+}
 
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', ()=> {
+        activeTab(index);
+    })
+})
