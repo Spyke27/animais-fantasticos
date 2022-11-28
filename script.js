59f92bb0ffb1ce934b54867856daf69f2f04ce8a
Rodrigo Daniel <rodrigoeufrasio2@gmail.com>
2022-11-28 18:52:52 -0300

    js faq

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function initTabNav (){
 const tabMenu = document.querySelectorAll('.js-tabmenu li')
 const tabCont = document.querySelectorAll('.js-tabcont section')
 
@@ -12,4 +13,20 @@ tabMenu.forEach((itemMenu, index) => {
     itemMenu.addEventListener('click', ()=> {
         activeTab(index);
     })
+});
+}
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+const activeClass = 'ativo';
+
+function activeAccordion(){
+    this.nextElementSibling.classList.toggle(activeClass);
+    this.classList.toggle(activeClass);
+}
+
+accordionList.forEach((item) => {
+    item.addEventListener('click', activeAccordion)
 })
+
+
